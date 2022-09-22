92eb925e0de781b74401b8391020ece6c50dab07
Vasanthakumar <vasanthakumara117@gmail.com>
2022-09-22 16:14:22 +0530

    refactored bracket

---
+++ Brackets_matching/bracket_match.rb
@@ -0,0 +1,31 @@
+def validate_bracket? bracket_array
+  bracket_hash = {'('=>')','['=>']','{'=>'}'}
+  store_brackets = []
+
+  bracket_array.each_char do |bracket|
+
+    if bracket_hash.has_key?(bracket)
+      store_brackets<<bracket
+    end
+
+    if bracket_hash.has_value?(bracket)
+      store_brackets.delete(bracket_hash.key(bracket))
+    end
+  end
+
+  print "Final : ",store_brackets,"\n"
+
+  if store_brackets.empty?
+    puts true
+  else
+    puts false
+  end
+
+end
+
+
+validate_bracket?("(){}[])")       # True
+validate_bracket?("([{}])")        # True
+validate_bracket?("(}")            # False
+validate_bracket?("[(])")          # False
+validate_bracket?("[({})](]")      # False
